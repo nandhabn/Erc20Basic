641da859360e6e1ea1e17835c37ccd7e99e4edc8
nandhabn <nandhub9@gmail.com>
2022-11-17 04:10:23 +0530

    #feature - s3 image upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "antd/dist/antd.css";
 import "bootstrap";
 import "./App.css";
 import ContractDetails from "./components/ContractDetails/ContractDetails";
 import TokenTransfer from "./components/TokenTransfer/TokenTransfer";
 import Balance from "./components/Balance/Balance";
+import ImageUpload from "./components/ImageUpload/imageUpload";
+import { getSignedS3url, SignType } from "./utils/axios/s3.service";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [imageViewUrl, setImageViewUrl] = useState("");
+
+  const imageReload = useCallback(() => {
+    getSignedS3url("file/image.jpeg", "image/jpeg", SignType.get).then(
+      (res) => {
+        setImageViewUrl(res);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getSignedS3url("file/image.jpeg", "image/jpeg", SignType.put).then(
+      (res) => {
+        setImageUrl(res);
+      }
+    );
+
+    imageReload();
+  }, [imageReload]);
+
   return (
     <div className="App">
       <div className="row h-100 flex align-content-center">
@@ -23,6 +46,8 @@ function App() {
         <div className="col-4" />
         <div className="col-4">
           <Balance />
+          <ImageUpload preSignedUrl={imageUrl} imageReload={imageReload} />
+          <img className="w-100" src={imageViewUrl} alt="nftimage" />
         </div>
         <div className="col-4" />
       </div>
